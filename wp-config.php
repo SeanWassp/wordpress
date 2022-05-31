ae4b7d8449b2c4964d14c7d2c68c0cb14adf62b0
Sean Coomer Wassp <105342613+SeanWassp@users.noreply.github.com>
2022-05-31 13:40:14 +0200

    Config DB
    config file updated DB

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'database_name_here' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'forge' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '2gsH2shV2L8Pt7Gg9NnQ' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
