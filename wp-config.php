203d1ec62731f91862b1692c393a798ab8940930
Sean Coomer Wassp <105342613+SeanWassp@users.noreply.github.com>
2022-05-31 13:50:52 +0200

    update DB
    update the Db credentials

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'forge' );
 
 /** Database username */
-define( 'DB_USER', 'forge' );
+define( 'DB_USER', 'testsite' );
 
 /** Database password */
-define( 'DB_PASSWORD', '2gsH2shV2L8Pt7Gg9NnQ' );
+define( 'DB_PASSWORD', 'testsite@2022' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
